e2b801e1b1828861f3ff4c568fb3b28a3fb34599
Adalid Claure <adalid.claure@rsa.com>
2019-12-11 09:29:52 -0500

    trying to work with this

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # conan-lua
+
+conan install .
+conan source .
+conan build . -bf=build
+conan package . -sf=source -bf=build -pf=package
+conan export-pkg . luadist/5.2.3@owl/stable
+conan upload luadist/5.2.3@owl/stable -r owl
--- conanfile.py
+++ conanfile.py
@@ -1,47 +1,37 @@
-from conans import ConanFile, CMake, tools
+# lua conan recipe 
+# as used by https://github.com/zethon/Owl
 
+from conans import ConanFile, CMake, tools
 
 class LuadistConan(ConanFile):
     name = "luadist"
     version = "5.2.3"
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Luadist here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
+    url = "https://github.com/LuaDist/lua"
+    license = "GPL"
+    description = "Lua is a powerful, efficient, lightweight, embeddable scripting language. It supports procedural programming, object-oriented programming, functional programming, data-driven programming, and data description."
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
-    default_options = {"shared": False}
+    default_options = "shared=False"
     generators = "cmake"
 
     def source(self):
-        self.run("git clone https://github.com/conan-io/hello.git")
-        # This small hack might be useful to guarantee proper /MT /MD linkage
-        # in MSVC if the packaged project doesn't have variables to set it
-        # properly
-        tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(HelloWorld)",
-                              '''PROJECT(HelloWorld)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
+        git = tools.Git(folder="source")
+        git.clone("https://github.com/LuaDist/lua.git", "master")
+        git.checkout("96245accd1539c8bb0d8be310eaa9cf5fbab9fed")
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="hello")
+        cmake.configure(source_folder="source")
         cmake.build()
 
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
-
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
+        for h in ["lauxlib.h", "lua.h", "lua.hpp", "lualib.h"]:
+            self.copy(h, dst="include/lua", src="source/src")
+        self.copy("luaconf.h", dst="include/lua", keep_path=False)
+        self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["hello"]
+        self.cpp_info.libs = ["liblua"]
 
